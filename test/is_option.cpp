bc95017235746ab6b87e37a0770b463a094a3387
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-10-06 21:08:14 +0900

    update test

--- test/is_option.cpp
+++ test/is_option.cpp
@@ -29,3 +29,31 @@ TEST(single, upper)
 		EXPECT_EQ(cbox::is_option("-" + test_char), test_char);
 	}
 }
+
+TEST(two, lower)
+{
+	for (int i = 0; i <= ('z' - 'a'); ++i)
+	{
+		std::string test_char = {(char)('a' + i)};
+		EXPECT_EQ(cbox::is_option("--" + test_char), test_char);
+	}
+}
+
+TEST(two, upper)
+{
+	for (int i = 0; i <= ('Z' - 'A'); ++i)
+	{
+		std::string test_char = {(char)('A' + i)};
+		EXPECT_EQ(cbox::is_option("--" + test_char), test_char);
+	}
+}
+
+TEST(invaid, only_minus)
+{
+	std::string test = "";
+	for (int i = 0; i < 2; ++i)
+	{
+		EXPECT_EQ(cbox::is_option(test), "");
+		test += "-";
+	}
+}
